24f2e7c45eb53a561a429925d8b8d38bfbfd6dab
Purvesh Dhaker <76883472+ashdhaker@users.noreply.github.com>
2023-08-30 14:36:53 +0530

    Update selection_sort.js

--- scripts/selection_sort.js
+++ scripts/selection_sort.js
@@ -1,11 +1,11 @@
 function Selection_sort()
 {
-    //Setting Time complexities
+    
     document.getElementById("Time_Worst").innerText="O(N^2)";
     document.getElementById("Time_Average").innerText="Θ(N^2)";
     document.getElementById("Time_Best").innerText="Ω(N^2)";
 
-    //Setting Space complexity
+    
     document.getElementById("Space_Worst").innerText="O(1)";
 
     c_delay=0;
