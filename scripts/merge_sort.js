2c5d28bf5f189b19473978208e65bf291287e53c
Purvesh Dhaker <76883472+ashdhaker@users.noreply.github.com>
2023-08-30 14:35:40 +0530

    Update merge_sort.js

--- scripts/merge_sort.js
+++ scripts/merge_sort.js
@@ -1,11 +1,11 @@
 function Merge()
 {
-    //Setting Time complexities
+    
     document.getElementById("Time_Worst").innerText="O(N log N)";
     document.getElementById("Time_Average").innerText="Θ(N log N)";
     document.getElementById("Time_Best").innerText="Ω(N log N)";
 
-    //Setting Space complexity
+    
     document.getElementById("Space_Worst").innerText="O(N)";
 
     c_delay=0;
